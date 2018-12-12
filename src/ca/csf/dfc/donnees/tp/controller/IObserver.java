4970fad36e2e4732e4bb5cc8527f6ea0ecabadb9
Max <max_gagne12345@hotmail.com>
2018-12-12 17:00:49 -0500

    Commentaires Max

--- src/ca/csf/dfc/donnees/tp/controller/IObserver.java
+++ src/ca/csf/dfc/donnees/tp/controller/IObserver.java
@@ -3,6 +3,16 @@ package ca.csf.dfc.donnees.tp.controller;
 import ca.csf.dfc.donnees.tp.model.IEspaceTravail;
 
 public interface IObserver {
+	/**
+	 * Compare l'espace de travail en mémoire avec un autre espace
+	 * de travail
+	 * @param 	p_EspaceTravail		Espace de travail à comparer
+	 * @return 	true si les espaces de travail sont identiques
+	 */
 	boolean Comparer(IEspaceTravail p_EspaceTravail);
+	/**
+	 * Met en mémoire un espace de travail
+	 * @param 	p_EspaceTravail		Espace de travail à mettre en mémoire
+	 */
 	public void SetEnregistrement(IEspaceTravail p_EspaceTravail);
 }
--- src/ca/csf/dfc/donnees/tp/view/IDessin.java
+++ src/ca/csf/dfc/donnees/tp/view/IDessin.java
@@ -3,11 +3,40 @@ package ca.csf.dfc.donnees.tp.view;
 import ca.csf.dfc.donnees.tp.controller.IForme;
 
 public interface IDessin {
+	/**
+	 * Permet de créer un nouvel espace de travail
+	 * @param	p_Largeur	Largeur du nouvel espace de travail
+	 * @param	p_Hauteur	Hauteur du nouvel espace de travail
+	 */
 	public void CreerEspaceTravail(int p_Largeur, int p_Hauteur);
+	/**
+	 * Verifie si l'espace de travail courant est le même que l'espace de travail du
+	 * dernier enregistrement
+	 * @return	true si les enregistrements sont identiques
+	 */
 	public boolean VerifierModification();
+	/**
+	 * Fait une sauvegarde de l'espace de travail courant
+	 * @return	true si l'enregistrement s'est bien déroulé
+	 */
 	public boolean Sauvegarder();
+	/**
+	 * Charge un espace de travail précédemment enregistré
+	 * @return  true si le chargement s'est bien déroulé
+	 */
 	public boolean Charger();
+	/**
+	 * Exporte un espace de travail en format visualisable
+	 * @return 	true si l'exportation s'est bien déroulé
+	 */
 	public boolean Exporter();
+	/**
+	 * Supprime la forme sélectionnée s'il y en a une
+	 */
 	public void Supprimer();
+	/**
+	 * Ajoute une forme dans l'espace de travail
+	 * @param 	p_Forme		Forme à ajouter à l'espace de travail
+	 */
 	public void AjouterForme(IForme p_Forme);
 }
